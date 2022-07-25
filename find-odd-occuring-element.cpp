324fc1921092f3e24ef4f14eec13f756caac817f
Hritik <pan@Hrk.local>
2022-07-25 23:17:24 +0530

    Bit Manipulation

---
+++ find-odd-occuring-element.cpp
@@ -0,0 +1,18 @@
+// importing libraries
+#include <bits/stdc++.h>
+
+/* Find the odd occurring element where 
+every element occurs even times. */
+void findOddOccuring(int *arr, int arrLen){
+
+    // now xor all the elements in array
+        int oddOcrEle = 0;
+
+        for(int index=0; index<arrLen; index++){
+            oddOcrEle ^= arr[index];
+        }
+
+        // now we have odd occuring element
+        std :: cout << oddOcrEle << std :: endl;
+}
+
