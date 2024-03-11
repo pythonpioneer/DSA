70304c7aad3e3454c79545b3a800ca2949812412
Hritik <pypi.pioneer@gmail.com>
2024-03-11 14:51:57 +0530

    reverse arr problem solved

--- problemsJS/array/reverseArr.js
+++ problemsJS/array/reverseArr.js
@@ -1,4 +1,5 @@
-/* write a function to reverse an array w.r.t input 
+/* Question 1
+* write a function to reverse an array w.r.t input 
     Test Case:
         arr = 1 2 3 4 5 6 
         n = 2
@@ -9,6 +10,24 @@
         n = 3
         ouput = 1 6 4 2 5
 */
-export const reverseArr = (arr, n) => {
-    return [3, 4, 5, 6, 1, 2];
+const reverseArr = (arr, n) => {
+
+    // rotating left and right part of the array
+    reverse(arr, 0, n-1)
+    reverse(arr, n, arr.length-1);
+
+    // reverse and return the whole arr
+    reverse(arr, 0, arr.length-1);
+    return arr;
 }
+
+// fully reverse the arr 1 2 3 4 5 6
+const reverse = (arr, i, j) => {
+
+    // traverse in the arr
+    for (; i<=j; i++, j--) {
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+}
+
+module.exports = { reverseArr };
