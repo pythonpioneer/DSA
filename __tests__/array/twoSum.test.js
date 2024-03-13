8a33c86e1c2412e690781b3b474c7631c7863fe3
Hritik <pypi.pioneer@gmail.com>
2024-03-13 18:11:05 +0530

    two sum done

--- __tests__/array/twoSum.test.js
+++ __tests__/array/twoSum.test.js
@@ -9,5 +9,14 @@ describe('Testing the two sum function with sorted array', () => {
         const expectedOutput = [1, 3];
 
         expect(twoSum(arr, sum)).toEqual(expectedOutput);
-    })
+    });
+    test('Testing when sum is not present', () => {
+
+        const arr = [2, 3, 5, 6, 11];
+        const sum = 10;
+        const expectedOutput = [-1];
+
+        expect(twoSum(arr, sum)).toEqual(expectedOutput);
+    });
+
 })
